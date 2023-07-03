3b7d29f030de5f064f5a945aea756688e86c8901
ammar <ammarfarghani@gmail.com>
2023-07-03 23:12:14 +0700

    add friend feature

---
+++ src/app/dashboard/add/page.tsx
@@ -0,0 +1,12 @@
+import AddFriendButton from '@/components/AddFriendButton'
+import { FC } from 'react'
+
+interface pageProps {
+
+}
+
+const page: FC<pageProps> = ({ }) => {
+  return <AddFriendButton></AddFriendButton>
+}
+
+export default page
