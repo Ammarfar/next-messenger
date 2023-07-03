73afb698dd0dda80d63727b855f2b019d9a841fb
ammar <ammarfarghani@gmail.com>
2023-07-03 23:11:35 +0700

    db & tailwind init, auth, button comp

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import { authOptions } from '@/lib/auth'
+import { getServerSession } from 'next-auth'
+import { FC } from 'react'
+
+interface pageProps {
+
+}
+
+const page: FC<pageProps> = async ({ }) => {
+
+  const session = await getServerSession(authOptions);
+
+  return <>
+    <div>{JSON.stringify(session)}</div>
+  </>
+}
+
+export default page
